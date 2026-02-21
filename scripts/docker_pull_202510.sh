da83a8168ffa7ed4e832011cb0325473a20cd442
Yuki Imajuku <yuki.imjk@gmail.com>
2026-02-21 23:47:17 +0900

    add docker related scripts

--- scripts/docker_pull_202510.sh
+++ scripts/docker_pull_202510.sh
@@ -9,7 +9,9 @@ docker image pull ${remote}:csharp-202510
 docker image pull ${remote}:fish-202510
 docker image pull ${remote}:fortran-202510
 docker image pull ${remote}:go-202510
+docker image pull ${remote}:haskell-202510
 docker image pull ${remote}:javascript-202510
+docker image pull ${remote}:julia-202510
 docker image pull ${remote}:lean-202510
 docker image pull ${remote}:ocaml-202510
 docker image pull ${remote}:perl-202510
@@ -18,3 +20,4 @@ docker image pull ${remote}:python-202510
 docker image pull ${remote}:rust-202510
 docker image pull ${remote}:typescript-202510
 docker image pull ${remote}:all-202510
+docker image pull ${remote}:all-lean-202510
